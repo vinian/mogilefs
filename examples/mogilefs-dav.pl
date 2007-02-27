b3bf21bbdf19bbc250f16679879c50ae6b9b864e
hachi <hachi@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2007-02-27 20:48:40 +0000

    This is a partially finished server implemented using Net::DAV::Server and the FilePaths plugin for MogileFS.
    It's all partially implemented, but it runs and does show good results, even if most of the results are wrong for one reason or another.
    
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@748 f67b2e87-0811-0410-a7e0-dd94e48410d6

---
+++ examples/mogilefs-dav.pl
@@ -0,0 +1,195 @@
+#!/usr/bin/perl
+
+# hachi 20070227
+# This is a partially finished server implemented using Net::DAV::Server and the FilePaths plugin for MogileFS.
+
+use strict;
+use warnings;
+
+use HTTP::Daemon;
+#use Filesys::Virtual::MogileFS; # Below for the time being
+use Net::DAV::Server;
+
+my $filesys = Filesys::Virtual::MogileFS->new;
+my $webdav = Net::DAV::Server->new();
+
+$webdav->filesys($filesys);
+
+my $d = HTTP::Daemon->new(
+    LocalAddr => '0.0.0.0',
+    LocalPort => 8008,
+    ReuseAddr => 1) || die $!;
+
+print $d->url . "\n";
+
+while (my $c = $d->accept) {
+    while (my $request = $c->get_request) {
+        my $response = eval { $webdav->run($request) };
+        warn "EVAL: $@" if $@;
+        $c->send_response($response);
+    }
+    $c->close;
+    undef $c;
+}
+
+package Filesys::Virtual::MogileFS;
+
+use strict;
+use warnings;
+
+use lib 'devel/mogilefs/api/perl/lib';
+
+use base 'Filesys::Virtual';
+
+use Fcntl qw(:mode);
+use MogileFS::Client;
+use Tie::Handle::HTTP;
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, (ref $class || $class);
+
+    my $mogclient = MogileFS::Client->new(
+        hosts => ['127.0.0.1:7001'],
+        domain => "danga.com::lj",
+    );
+
+    die unless $mogclient;
+
+    $self->{mogclient} = $mogclient;
+
+    return $self;
+}
+
+sub mogclient { return $_[0]->{mogclient}; }
+
+sub open_write {
+    my $self = shift;
+    my $path = shift;
+
+    my $handle = $self->mogclient->new_file($path, 'temp', 1024);
+    warn "Handle bad!!!!" unless $handle;
+    return $handle;
+}
+
+sub close_write {
+    my $self = shift;
+    my $handle = shift;
+    close $handle;
+}
+
+sub open_read {
+    my $self = shift;
+    my $path = shift;
+
+    my @paths = $self->mogclient->get_paths($path);
+
+    return unless @paths;
+
+    my $handle = Tie::Handle::HTTP->new($paths[0]);
+
+    return $handle;
+}
+
+sub close_read {
+    my $self = shift;
+    my $handle = shift;
+
+    close $handle;
+}
+
+sub list {
+    my $self = shift;
+    my $path = shift;
+
+    my $listing = $self->mogclient->list_directory($path);
+
+    return keys %$listing if $listing;
+
+    return () if $path eq '/';
+
+    return;
+}
+
+my @dir_stat = (
+    e => 1,
+    d => 1,
+);
+
+my @file_stat = (
+    e => 1,
+    d => 0,
+    s => 1025,
+);
+
+sub MODE_DIR  () { S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO }
+sub MODE_FILE () { S_IFREG | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH }
+
+sub stat {
+    my $self = shift;
+    my $path = shift;
+
+    my $mogclient = $self->mogclient;
+
+    my @paths = $mogclient->get_paths($path);
+
+    if (@paths) {
+        return ($$, 0, MODE_FILE, 1, 0, 0, undef, 1024, time, $^T, $^T, 512, 512);
+    }
+
+    my $listing = $mogclient->list_directory($path);
+
+    if ($listing || $path eq '/') {
+        return ($$, 0, MODE_DIR, 1, 0, 0, undef, 1024, time, $^T, $^T, 512, 512);
+    }
+
+    return;
+}
+
+<<EOT;
+0 dev
+1 ino
+2 mode
+3 nlink
+4 uid
+5 gid
+6 rdev
+7 size
+8 atime
+9 mtime
+10 ctime
+11 blksize
+12 blocks
+EOT
+
+
+sub test {
+    my $self = shift;
+    my ($test, $path) = @_;
+
+    my @stat = $self->stat($path);
+
+    return 0 unless @stat;
+
+    my $tests = {
+        'e' => sub {
+            return 1;
+        },
+        's' => sub {
+            return $stat[7];
+        },
+        'f' => sub {
+            return S_ISREG($stat[2]);
+        },
+        'd' => sub {
+            return S_ISDIR($stat[2]);
+        },
+    };
+
+    return $tests->{$test}->()
+        if exists $tests->{$test};
+
+    warn "No test defined for $test on file $path\n";
+}
+
+1;
