6c34d3494b624a5cd27e9864df91177ef5935780
dormando <dormando@f67b2e87-0811-0410-a7e0-dd94e48410d6>
2008-11-15 00:58:24 +0000

    add replicate_order_final_choices hook.
    ... filter by specified local network address.
    might have a bug if no such zone supplied? I think the rest of the plugin has
    the same bug...
    
    git-svn-id: http://code.sixapart.com/svn/mogilefs/trunk@1234 f67b2e87-0811-0410-a7e0-dd94e48410d6

--- server-plugins/MogileFS-Plugin-ZoneLocal/lib/MogileFS/Plugin/ZoneLocal.pm
+++ server-plugins/MogileFS-Plugin-ZoneLocal/lib/MogileFS/Plugin/ZoneLocal.pm
@@ -18,8 +18,10 @@ sub load {
         my $devices = shift;
         my $sorted_devs = shift;
 
-        @$sorted_devs = prioritize_devs_current_zone
-                        MogileFS::Worker::Query::sort_devs_by_utilization(@$devices);
+        @$sorted_devs = prioritize_devs_current_zone(
+                        $MogileFS::REQ_client_ip,
+                        MogileFS::Worker::Query::sort_devs_by_utilization(@$devices)
+                        );
 
         return 1;
     });
@@ -28,8 +30,22 @@ sub load {
         my $devices = shift;
         my $sorted_devs = shift;
 
-        @$sorted_devs = prioritize_devs_current_zone
-                        MogileFS::Worker::Query::sort_devs_by_freespace(@$devices);
+        @$sorted_devs = prioritize_devs_current_zone(
+                        $MogileFS::REQ_client_ip,
+                        MogileFS::Worker::Query::sort_devs_by_freespace(@$devices)
+                        );
+
+        return 1;
+    });
+
+    MogileFS::register_global_hook( 'replicate_order_final_choices', sub {
+        my $devs    = shift;
+        my $choices = shift;
+
+        my @sorted = prioritize_devs_current_zone(
+                     MogileFS::Config->config('local_network'),
+                     map { $devs->{$_} } @$choices);
+        @$choices  = map { $_->id } @sorted;
 
         return 1;
     });
@@ -41,12 +57,14 @@ sub unload {
     # remove our hooks
     MogileFS::unregister_global_hook( 'cmd_get_paths_order_devices' );
     MogileFS::unregister_global_hook( 'cmd_create_open_order_devices' );
+    MogileFS::unregister_global_hook( 'replicate_order_final_choices' );
 
     return 1;
 }
 
 sub prioritize_devs_current_zone {
-    my $current_zone = MogileFS::Network->zone_for_ip($MogileFS::REQ_client_ip);
+    my $local_ip = shift;
+    my $current_zone = MogileFS::Network->zone_for_ip($local_ip);
     my (@this_zone, @other_zone);
 
     foreach my $dev (@_) {
